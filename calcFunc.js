79d4f9ed7529c50c910d47efb0d385f6dd2c673e
PrateekP <prateekpanta1789@gmail.com>
2024-08-24 23:13:47 +0530

    Added intermediate functionality

---
+++ calcFunc.js
@@ -0,0 +1,69 @@
+let output = 0;
+let inputEquation = ``;
+let currentNum = ``;
+
+const displayInput = document.querySelector("#input");
+const displayOutput = document.querySelector("#output");
+
+const allClear = document.querySelector(".allClear");
+const deleteNum = document.querySelector(".delete");
+const modulo = document.querySelector(".modulo");
+
+const btn = [...document.querySelectorAll(`.btn`)];
+/* const btnNum = [...document.querySelectorAll(".operand")];
+const btnOperator = [...document.querySelectorAll(".operator")]; */
+
+btn.forEach(itm => {
+    itm.addEventListener("click", () => {
+        if (Number(itm.dataset.value) || Number(itm.dataset.value) == 0 || itm.dataset.value == '.') {
+            currentNum += itm.dataset.value;
+            inputEquation += itm.dataset.value;
+            displayOutput.value = Number(currentNum);
+            displayInput.value = inputEquation;
+        }
+        else if (!Number(itm.dataset.operator)) {
+            inputEquation += `${itm.dataset.operator}`;
+            displayInput.value = inputEquation;
+            currentNum = '';
+        }
+
+        /* if (itm.dataset.operator == '/' && displayInput.value == '' && displayOutput.value == '') {
+            inputEquation += '/';
+        } */
+        /* if (itm.dataset.value == '+/-') {
+            if (currentNum > 0) {
+                currentNum / -1;
+                displayOutput.value = currentNum;
+            }
+            else {
+                currentNum = currentNum + currentNum * 2;
+            }
+        } */  
+
+        if (itm.dataset.operator == '=') {
+            const arrEquation = [...inputEquation];
+            arrEquation.pop();
+            displayOutput.value = eval(arrEquation.join(''));
+        }
+    })
+})
+
+deleteNum.addEventListener("click", () => {
+    let arrayEquation = [...inputEquation];
+    arrayEquation.length != [] ? arrayEquation.pop() : arrayEquation = [];
+    inputEquation = arrayEquation.join('');
+    displayInput.value = inputEquation;
+
+    let arrayNum = [...currentNum];
+    arrayNum.length != [] ? arrayNum.pop() : arrayNum = [];
+    displayOutput.value = displayOutput.value.slice(0, -1);
+    currentNum = arrayNum.join('');
+    displayOutput.value = currentNum;
+})
+
+allClear.addEventListener("click", () => {
+    displayInput.value = '';
+    displayOutput.value = '';
+    currentNum = '';
+    inputEquation = '';
+})
