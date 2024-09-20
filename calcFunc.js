21314f88b24dc73262b4578ef022d2d5e59900ae
PrateekP <prateekpanta1789@gmail.com>
2024-09-20 17:39:06 +0530

    improved upon deleting algotithm and fixed the issue where opon clicking a operator would show undefined on input display

--- calcFunc.js
+++ calcFunc.js
@@ -9,67 +9,61 @@ const displayOutput = document.querySelector("#output");
 const allClear = document.querySelector(".allClear");
 const deleteNum = document.querySelector(".delete");
 const modulo = document.querySelector(".modulo");
+const convert = document.querySelector(".btn-X");
 
-const btn = [...document.querySelectorAll(`.btn`)];
-const convert = document.querySelector(".conversion");
+/* const btn = [...document.querySelectorAll(`.btn`)]; */
+const operand = [...document.querySelectorAll(`.operand`)];
+const operator = [...document.querySelectorAll(`.operator`)];
 
-btn.forEach(itm => {
+operand.forEach(itm => {
     itm.addEventListener("click", () => {
-        if (Number(itm.dataset.value) || Number(itm.dataset.value) == 0 || itm.dataset.value == '.') {
-            currentNum += itm.dataset.value;
-            inputEquationShow += itm.dataset.value;
-            inputEquationEval += itm.dataset.value;
-            displayOutput.value = currentNum;
-            displayInput.value = inputEquationShow;
-        }
-        else if (!Number(itm.dataset.operator)) {
-            inputEquationShow += `${itm.textContent}`;
-            inputEquationEval += `${itm.dataset.operator}`;
-            console.log(inputEquationEval);
-            displayInput.value = inputEquationShow;
-            currentNum = ``;
-        }
-
-        if (itm.dataset.operator == '=') {
-            const arrEquation = [...inputEquationEval];
-            arrEquation.pop();
-            displayOutput.value = eval(arrEquation.join(''));
-            currentNum = ``;
-            inputEquationShow = ``;
-            inputEquationEval = ``;
-        }
+        currentNum += itm.dataset.value;
+        displayOutput.value = currentNum;
+        inputEquationShow += itm.dataset.value;
+        inputEquationEval += itm.dataset.value;
+        displayInput.value = inputEquationShow;
     })
 })
 
-convert.addEventListener("click", () => {
-    if (!displayOutput.value && !displayInput.value) {
-        return; 
-    }
-    else if (displayOutput.value && displayOutput.value > 0) {
-        currentNum = displayOutput.value / -1;
-        displayOutput.value = currentNum;
-        inputEquationShow += `${currentNum}`;
-        console.log(inputEquationShow);
-        displayInput.value = inputEquationShow;
-    }
-    else if (displayOutput.value && displayOutput.value < 0) {
-        currentNum = displayOutput.value * -1;
-        displayOutput.value = currentNum;
-    }
+operator.forEach(itm => {
+    itm.addEventListener("click", () => {
+        if (itm.dataset.operator != '=') {
+            inputEquationShow += itm.dataset.operator;
+            inputEquationEval += itm.dataset.operator
+            displayInput.value = inputEquationShow;
+            currentNum = ``;
+        }
+        else {
+            displayOutput.value = eval(inputEquationEval);
+            currentNum = displayOutput.value;
+            /* currentNum = ``;
+            inputEquationShow = ``;
+            inputEquationEval = ``; */
+        }
+    })  
 })
 
 deleteNum.addEventListener("click", () => {
     let arrayEquation = [...inputEquationShow];
     arrayEquation.length != [] ? arrayEquation.pop() : arrayEquation = [];
     inputEquationShow = arrayEquation.join('');
+    inputEquationEval = inputEquationShow;
     displayInput.value = inputEquationShow;
 
     let arrayNum = [...currentNum];
     arrayNum.length != [] ? arrayNum.pop() : arrayNum = [];
-    displayOutput.value = displayOutput.value.slice(0, -1);
     currentNum = arrayNum.join('');
     displayOutput.value = currentNum;
 })
+
+/* convert.addEventListener("click", () => {
+    let newNum = Number(currentNum);
+    console.log(newNum);
+    if (newNum > 0) {
+        currentNum = newNum / -1;
+        displayOutput.value = currentNum;
+    }
+}) */
 
 allClear.addEventListener("click", () => {
     displayInput.value = ``;
